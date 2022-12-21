9db2f78150bdd7c262e51d190e2d91e08e275d76
Mario Valencia <iaivalencia@hotmail.com>
2022-12-20 21:40:06 -0500

    adding new modifications such as burger menu

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector('.burger-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     console.log('click');
     desktopMenu.classList.toggle('inactive');
 }
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
+}
 
-console.log("js funcionando");
+
+
+
+
